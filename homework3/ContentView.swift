a99e8879c0ec1c1e3ad410d1e5d4f835cf88e52d
dejes <jack940197@gmail.com>
2020-10-28 15:43:00 +0800

    Initial Commit

---
+++ homework3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  homework3
+//
+//  Created by User14 on 2020/10/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ homework3/homework3App.swift
@@ -0,0 +1,17 @@
+//
+//  homework3App.swift
+//  homework3
+//
+//  Created by User14 on 2020/10/28.
+//
+
+import SwiftUI
+
+@main
+struct homework3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
